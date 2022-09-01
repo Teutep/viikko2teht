73a2af0bb0aebff930f98f832d7e4808397cd7fa
Teuvo <teuvo.almgren@gmail.com>
2022-09-01 12:59:36 +0300

    Kouluteht

--- teht3.4.py
+++ teht3.4.py
@@ -1,6 +1,13 @@
 Vuosi = int(input("Onko tämä vuosi karkausvuosi?: "))
-Karkausvuosi = Vuosi/4
-if Karkausvuosi == int(Karkausvuosi):
-    print("Kyllä on")
+Jaollinen4 = Vuosi % 4
+Jaollinen100 = Vuosi % 100
+Jaollinen400 = Vuosi % 400
+if Jaollinen4 == 0:
+    if Jaollinen100 == 0 and Jaollinen400 == 0:
+        print("Kyllä on")
+    elif Jaollinen100 == 0 and Jaollinen400 > 0:
+        print("Ei ole")
+    else:
+        print("Kyllä on")
 else:
     print("Ei ole")
