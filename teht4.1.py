c8617b9afefc83a8821758ac16634b9181109813
Teuvo <teuvo.almgren@gmail.com>
2022-09-01 13:36:04 +0300

    Kouluteht

---
+++ teht4.1.py
@@ -0,0 +1,8 @@
+#Tehtävänanto on kirjoittaa while toistorakenne, joka tulostaa kolmella jaolliset 1 ... 1000
+i = 3
+print(f"i = {i}")
+while i < 999:
+    i += 3
+    print(f"i = i + 3 = {i}")
+else:
+    print("Testi on ohitse")
